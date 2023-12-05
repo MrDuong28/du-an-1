18a2660800ce8f72e233c7a48b0d506cb4dfb689
nguyentien14 <144757075+nguyentien14@users.noreply.github.com>
2023-12-05 21:47:38 +0700

    update chi_tiet

--- chi_tiet.php
+++ chi_tiet.php
@@ -16,73 +16,94 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>TDT FASHION</title>
-		<link rel="stylesheet" type="text/css" href="giao_dien/css.css">
-      
+		<link rel="stylesheet" href="giao_dien/csss.css">
+		<style>
+    /* Form */
+    form {
+      width: 500px;
+      margin: 0 auto;
+      padding: 20px;
+      background-color: #fff;
+      border: 1px solid #ccc;
+    }
+
+    /* Tiêu đề form */
+    h1 {
+      text-align: center;
+      font-size: 20px;
+      font-weight: bold;
+    }
+
+    /* Các trường input */
+    input, textarea {
+      width: 100%;
+      padding: 10px;
+      border: 1px solid #ccc;
+      margin-bottom: 10px;
+    }
+
+    /* Nút submit */
+    input[type="submit"] {
+      background-color: #000;
+      color: #fff;
+      border: none;
+      padding: 10px;
+      cursor: pointer;
+    }
+
+    /* Icon */
+    .icon {
+      width: 20px;
+      height: 20px;
+      margin-right: 10px;
+    }
+  </style>
+
 	</head>
 	<body>
-    	
 		<center>
-        	<div class="header">
-            	<div class="header_left">
-                	<ul>
-                    	<li><span  style="color:red;"class="glyphicon glyphicon-earphone"></span>  0914139010</li>
-                        
-                    </ul>
-                </div>
-                <div class="header_center">
-                	<?php include("chuc_nang/tim_kiem/vung_tim_kiem.php");?>
-                </div>
-                <div class="header_right">
-                    
-                         <a href="login_SignUp.php"> <span class="glyphicon glyphicon-user"></span>Tài Khoản</a>
-                    
-                </div>
-        	</div>
-            <div style=" text-align:center; margin-top:25px; margin-bottom:15px;">
-			<img style="height: 200px;" src="img/z4887413404811_1328bd9db9e2da99b79169c311b097ba.jpg"></a>
-            </div>
-			<div class="menu">
-           				<?php
-							include("chuc_nang/menu_ngang/menu_ngang.php");
-						?> 
-                        
-			</div>
-            <div class="conten">
-            		<div class="conten_left">
-                        <div class="nhungdanhmuc">
-                        	<h3 style="color: #000;">Danh mục sản phẩm</h3>
-                            <div class="noidung" style="text-align:left;">
-                            	<?php include("chuc_nang/menu_doc/menu_doc.php"); ?>
-                             </div>   
-                        </div>   
-                        
-                        <div class="nhungdanhmuc">
-						<h3 style="color: #000;">Sản phẩm nổi bật</h3>
-                            
-                        	<?php include("chuc_nang/san_pham/noi_bat.php");?>
-                            </div>
-						</div>		
-                        
-                    </div>
-                    
-                    <div class="conten_right">
-                    		<div style=" color:#000; text-align:justify;">
-                            	<h4>Địa chỉ:</h4>
-                                THỜI TRANG TDT<br>
-								FPT Polytechnic, Tòa nhà, P. Trịnh Văn Bô, Nam Từ Liêm, Hà Nội <br>
-                            </div>
-                			<br>
-							<div style=" width:780">
-							<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d930.9664907608384!2d105.74696120499803!3d21.038048500736956!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313455305afd834b%3A0x17268e09af37081e!2sT%C3%B2a%20nh%C3%A0%20FPT%20Polytechnic.!5e0!3m2!1svi!2s!4v1699847648561!5m2!1svi!2s" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-                            </div>
-                            
+        <div class="header_left">
+          <ul>
+            <li><span  style="color:red;"class="glyphicon glyphicon-earphone"></span>Hotline: 0914139010</li>
+          </ul>
+        </div>
+        
+			  <div class="menu">
+          <?php include("chuc_nang/menu_ngang/menu_ngang.php");?>            
+			  </div>
+
+          <div class="conten_right" style="text-align:justify; color:#000;">
+                <div class=gioithieu style="padding:10px; color:#6A6A6A;">
+                    <div style="font-weight:bold; font-size:25px; color:#000;">TDT FASHION</div><br>
+					<form action="mailto:your_email_address" method="post">
+    <div class="form-group">
+      <input type="text" name="name" placeholder="Họ tên" required>
+      <i class="icon fas fa-user"></i>
+    </div>
+    <div class="form-group">
+      <input type="email" name="email" placeholder="Email" required>
+      <i class="icon fas fa-envelope"></i>
+    </div>
+    <div class="form-group">
+      <input type="text" name="subject" placeholder="Tiêu đề">
+      <i class="icon fas fa-tag"></i>
+    </div>
+    <div class="form-group">
+      <textarea name="message" placeholder="Nội dung"></textarea>
+      <i class="icon fas fa-file-alt"></i>
+    </div>
+    <input type="submit" value="Gửi">
+  </form>
+							<br>
+							<br>
+                          </div>  
                 	</div>		
             </div>
-            <!-- end conten--->
-				<?php include('footer.php');?>	
-					
-                    
-				
+				<div class="footer">
+					<?php include('footer.php');?>
+				</div>
+
+
 		</center>
 	</body>
 </html>
