d99eaa0b1624aab233b439dc3e54a03bc3ac80a9
nguyentien14 <144757075+nguyentien14@users.noreply.github.com>
2023-12-10 17:36:11 +0700

    upload

---
+++ quan_tri/chuc_nang/menu_doc/them_menu_doc.php
@@ -0,0 +1,24 @@
+<?php 
+	if(!isset($bien_bao_mat)){exit();}
+?>
+<form action="" method="post">
+	<table width="990px" >
+		<tr>
+			<td colspan="2" ><b style="color:black;font-size:20px" >Thêm menu dọc</b><br><br> </td>
+			
+		</tr>
+		<tr>
+			<td>Tên danh mục: 
+			<input style="width:200px" name="ten" value="" >
+			</td>
+
+		</tr>
+		<tr>
+			
+			<td>
+				<br>
+				<input type="submit" name="bieu_mau_them_menu_doc" value="Thêm " style="width:55px;height:30px;font-size:16px" >
+			</td>
+		</tr>
+	</table>
+</form>
