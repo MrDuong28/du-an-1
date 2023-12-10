0e0c45b1ed4800bbc89b4188a4708a9ca274453a
nguyentien14 <144757075+nguyentien14@users.noreply.github.com>
2023-12-10 22:24:05 +0700

    update sua_thong_tin_quan_tri

--- quan_tri/chuc_nang/quan_tri_2/sua_thong_tin_quan_tri.php
+++ quan_tri/chuc_nang/quan_tri_2/sua_thong_tin_quan_tri.php
@@ -10,18 +10,18 @@
 <form action="" method="post" >
 	<table width="990px" >
 		<tr>
-			<td colspan="2" ><b style="color:blue;font-size:20px" >Sửa thông tin quản trị</b><br><br> </td>			
+			<td colspan="2" ><b style="color:black;font-size:20px">Sửa thông tin quản trị</b><br><br> </td>			
 		</tr>
 		<tr>
 			<td width="100px" >Ký danh : </td>
 			<td width="890px" >
-				<input style="width:400px;margin-top:3px;margin-bottom:3px;" name="ky_danh" value="<?php echo $ky_danh; ?>" >
+				<input style="width:400px;margin-top:3px;margin-bottom:3px;border-radius: 5px;" name="ky_danh" value="<?php echo $ky_danh; ?>" >
 			</td>
 		</tr>
 		<tr>
 			<td valign="top" >Mật khẩu : </td>
 			<td >
-				<input type="password" style="width:400px;margin-top:3px;margin-bottom:3px;" name="mat_khau" value="khong_doi" ><br><br>
+				<input type="password" style="width:400px;margin-top:3px;margin-bottom:3px;border-radius: 5px;" name="mat_khau" value="khong_doi" ><br><br>
 				- Nếu chỉ muốn đổi ký danh thì chỉ cần sửa ký danh , còn phần mật khẩu thì để nguyên (mật khẩu sẽ không đổi khi gửi biểu mẫu) <br><br>
 				- Khi sửa thông tin quản trị mà thông tin mật khẩu nhập vào là 'khong_doi' thì thông tin mật khẩu vẫn giữ nguyên (nghĩa là mật khẩu cũ không đổi)
 				<br><br>
@@ -32,7 +32,7 @@
 			<td>&nbsp;</td>
 			<td>
 				<br>
-				<input type="submit" name="bieu_mau_sua_thong_tin_quan_tri" value="Sửa" style="width:200px;height:50px;font-size:24px" >
+				<input type="submit" name="bieu_mau_sua_thong_tin_quan_tri" value="Sửa" style="width:55px;height:30px;font-size:16px" >
 			</td>
 		</tr>
 	</table>
